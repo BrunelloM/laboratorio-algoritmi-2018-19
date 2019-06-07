df3066a9a6b0597512504b4a5ae0b21d2f8382d4
Lorenzo Caresio <caresio.lorenzo@educ.di.unito.it>
2019-06-07 21:16:53 +0200

    Update start.sh

---
+++ Esercizio 4/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+rm -rf ./out/*;
+mkdir ./out/;
+cd src;
+javac -d ../out/ ./kruskalAlgorithmUsage/*.java || exit;
+javac -d ../out/ -cp ".:/graph.*:/usr/share/java/junit4.jar:/usr/share/java/hamcrest-core-1.3.jar:" ./test/*.java || exit;
--- Esercizio 4/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-rm -rf ./out/*;
-mkdir ./out/;
-cd src;
-javac -d ../out/ ./kruskalAlgorithmUsage/*.java || exit;
-javac -d ../out/ -cp ".:/graph.*:/usr/share/java/junit4.jar:" ./test/*.java || exit;
