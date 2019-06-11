89ad56ca630b3d957c31f2e8f6859bcbb79a89f4
Lorenzo <lorenzo.caresio@edu.unito.it>
2019-06-11 17:07:17 +0200

    Exercise 3 - Unit testing implemented (2)

--- Esercizio 3/src/test/UnionFindTest.java
+++ Esercizio 3/src/test/UnionFindTest.java
@@ -19,7 +19,7 @@ public class UnionFindTest {
   }
 
   @Test
-  public void makeSetTest(){
+  public void makeSetTest_true(){
     unionFind.makeSet(1);
     assertEquals(Integer.valueOf(1), unionFind.find(1));
   }
@@ -29,4 +29,19 @@ public class UnionFindTest {
     assertNotEquals(Integer.valueOf(1),unionFind.find(1));
   }
 
+  @Test
+  public void unionTest_true(){
+    unionFind.makeSet(1);
+    unionFind.makeSet(2);
+    unionFind.union(1, 2);
+    assertEquals(unionFind.find(1) ,unionFind.find(2));
+  }
+
+  @Test
+  public void unionTest_false(){
+    unionFind.makeSet(1);
+    unionFind.makeSet(2);
+    assertNotEquals(unionFind.find(1) ,unionFind.find(2));
+  }
+  
 }
